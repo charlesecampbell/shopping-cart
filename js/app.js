4220dc76151a7912bcddf12b2915db458c1ef5d2
ERIK M. DODD <emdodd27@gmail.com>
2022-09-01 14:25:04 -0700

    finished all To Do's on app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 // Cart constructor.
 const Cart = function(items) {
   // this.items is an array of CartItem instances.
@@ -8,16 +9,26 @@ const Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
-};
+  let newItem = new CartItem(product, quantity);
+  this.items.push(newItem);
+  };
+
+  
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  let toStore = newItem;
+  console.log(newItem);
+  localStorage.setItem('newItem', JSON.stringify(newItem));
 };
 
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+  localStorage.removeItem('item');
+  // console.log(toStore);
 };
+
 
 const CartItem = function(product, quantity) {
   this.product = product;
